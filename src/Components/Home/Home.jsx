0e407241ba653f7af9e856c8ecb81a5b8077535a
Moises Contreras <129238200+comicway@users.noreply.github.com>
2025-03-25 10:11:03 -0300

    termino home
    funcionalidad de seleccionar el nombre del rover

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,37 +1,44 @@
 import { useState } from "react";
 
 const Home = () => {
+    
+    const [roverSelect, setRoverSelect] = useState();
 
-    const [roverSelect, setRoverSelect] = useState('');
+    const selectRover = (selectedRover) => {
+        setRoverSelect(selectedRover);
+        localStorage.setItem('selectedRover', selectedRover);
+        console.log(selectedRover);
+    };
 
     return (
         <>
             <div className="grid grid-cols-1">
-                    <img src="/src/assets/react.svg" alt="" className="mx-auto mt-5" />
-                    <h1 className="text-center">Mars Rover Photos App</h1>
-                    <p className="text-center">¡Elige tu rover preferido!</p>
+                <img src="/src/assets/react.svg" alt="" className="mx-auto mt-5" />
+                <h1 className="text-center">Mars Rover Photos App</h1>
+                <p className="text-center">¡Elige tu rover preferido!</p>
+            </div>
+            <div className="grid sm:grid-cols-3 grid-cols-1 gap-3 mt-10">
+                <div>
+                    <button onClick={() => selectRover('curiosity')}>
+                        <img src="/src/assets/react.svg" alt=""/>
+                    </button>
+                    <p>Curiosity</p>
                 </div>
-                <div className="grid sm:grid-cols-3 grid-cols-1 gap-3 mt-10">
-                    <div>
-                        <button>
-                            <img src="/src/assets/react.svg" alt=""/>
-                        </button>
-                        <p>Curiosity</p>
-                    </div>
-                    <div>
-                        <button>
-                            <img src="/src/assets/react.svg" alt=""/>
-                        </button>
-                        <p>Opportunity</p>
-                    </div>
-                    <div>
-                        <button>
-                            <img src="/src/assets/react.svg" alt=""/>
-                        </button>
-                        <p>Spirit</p>
-                    </div>
+                <div>
+                    <button onClick={() => selectRover('opportunity')}>
+                        <img src="/src/assets/react.svg" alt=""/>
+                    </button>
+                    <p>Opportunity</p>
+                </div>
+                <div>
+                    <button onClick={() => selectRover('spirit')}>
+                        <img src="/src/assets/react.svg" alt=""/>
+                    </button>
+                    <p>Spirit</p>
+                </div>
             </div>
         </>
-    )
+    );
 };
+
 export default Home;
