a51fdc89affcf3568765c2418c1a4ff01d558246
Moises Contreras <129238200+comicway@users.noreply.github.com>
2025-04-22 10:36:17 -0400

    Instalacion de fuente y primeros estilos
    Instalacion de fuente y primeros estilos

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -14,9 +14,8 @@ const Home = () => {
     return (
         <>
             <div className="grid grid-cols-1">
-                <img src="/src/assets/react.svg" alt="" className="mx-auto mt-5" />
-                <h1 className="text-center">Mars Rover Photos App</h1>
-                <p className="text-center">¡Elige tu rover preferido!</p>
+                <img src="/src/assets/logoroverphotos.svg" alt="" className="mx-auto mt-5" />
+                <p className="text-center text-white border-t border-b border-white my-[32px] text-[20px] font-bold font-SpaceGrotesk">¡Elige tu rover preferido!</p>
             </div>
             <div className="grid sm:grid-cols-3 grid-cols-1 gap-3 mt-10">
                 <div>
