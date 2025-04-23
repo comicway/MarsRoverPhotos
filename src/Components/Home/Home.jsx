0fd764b5c6592cad322e95b86f02b0fdcf855bbd
Moises Contreras <129238200+comicway@users.noreply.github.com>
2025-04-23 13:53:57 -0400

    Avances del home
    Avances del home, fotos rovers, boton seleccionar

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,33 +15,41 @@ const Home = () => {
         <>
             <div className="grid grid-cols-1">
                 <img src="/src/assets/logoroverphotos.svg" alt="" className="mx-auto mt-5" />
-                <p className="text-center text-white border-t border-b border-white my-[32px] text-[20px] font-bold font-SpaceGrotesk">¡Elige tu rover preferido!</p>
+                <p className="text-center text-white border-t border-b border-white my-[32px] text-[20px] font-bold font-SpaceGrotesk py-[18px]">¡Elige tu rover preferido!</p>
             </div>
-            <div className="grid sm:grid-cols-3 grid-cols-1 gap-3 mt-10">
-                <div>
+            <div className="grid grid-cols-3 gap-3 mt-2">
+                <div className="mx-auto">
                     <button onClick={() => selectRover('curiosity')}>
-                        <img src="/src/assets/react.svg" alt=""/>
+                        <img src="/asset/img/rover-curiosity-3d.png" alt="Rover Curiosity"/>
                     </button>
-                    <p>Curiosity</p>
+                    <p className="text-center text-white font-medium font-SpaceGrotesk text-[15px]">Curiosity</p>
                 </div>
-                <div>
+                <div className="mx-auto">
                     <button onClick={() => selectRover('opportunity')}>
-                        <img src="/src/assets/react.svg" alt=""/>
+                        <img src="/asset/img/rover-opportunity-3d.png" alt="Rover Opportunity"/>
                     </button>
-                    <p>Opportunity</p>
+                    <p className="text-center text-white font-medium font-SpaceGrotesk text-[15px]">Opportunity</p>
                 </div>
-                <div>
+                <div className="mx-auto">
                     <button onClick={() => selectRover('spirit')}>
-                        <img src="/src/assets/react.svg" alt=""/>
+                        <img src="/asset/img/rover-spirit-3d.png" alt="Rover Spirit"/>
                     </button>
-                    <p>Spirit</p>
+                    <p className="text-center text-white font-medium font-SpaceGrotesk text-[15px]">Spirit</p>
                 </div>
             </div>
-            <div className="grid grid-cols-1">
-            <Link to='/calendar' className="ml-auto">
-                <button disabled={!roverSelect}>Siguiente</button>
-            </Link>  
+            <div className="grid grid-cols-1 mt-10">
+            <Link to='/calendar' className="block w-full">
+                <button
+                    disabled={!roverSelect}
+                    className="text-white font-bold font-SpaceGrotesk text-[16px] bg-[#BF3B0B] w-full h-[40px] rounded disabled:opacity-50 disabled:cursor-not-allowed">
+                    Seleccionar
+                </button>
+            </Link>
             </div>
+            <section className="grid grid-cols-1 mt-10">
+                <p className="text-center text-white border-t border-b border-white my-[32px] text-[18px] font-bold font-SpaceGrotesk py-[18px]"><span className="font-light">DÍA DE LANZAMIENTO: </span>25 JULIO 2013</p>
+                <p className="text-left text-white border-t border-b border-white text-[18px] font-light font-SpaceGrotesk py-[18px]">STATUS:</p>
+            </section>
         </>
     );
 };
