63f9e138163b888369f70497563c158d6940e243
Moises Contreras <129238200+comicway@users.noreply.github.com>
2025-04-24 13:30:29 -0400

    Maqueta info de los rovers
    Maqueta info de los rovers

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -47,8 +47,27 @@ const Home = () => {
             </Link>
             </div>
             <section className="grid grid-cols-1 mt-10">
-                <p className="text-center text-white border-t border-b border-white my-[32px] text-[18px] font-bold font-SpaceGrotesk py-[18px]"><span className="font-light">DÍA DE LANZAMIENTO: </span>25 JULIO 2013</p>
-                <p className="text-left text-white border-t border-b border-white text-[18px] font-light font-SpaceGrotesk py-[18px]">STATUS:</p>
+                <div className="text-center text-white border-t border-b border-white mt-[32px] text-[18px] font-bold font-SpaceGrotesk py-[18px]"><span className="font-light">DÍA DE LANZAMIENTO: </span>25 JULIO 2013</div>
+                <div className="flex justify-evenly text-white border-b border-white text-[18px] font-light font-SpaceGrotesk py-[18px]">
+                    <div>STATUS:</div>
+                    <div className="bg-[#0FF2F2] font-bold text-black px-2 text-center w-[200px]">ACTIVO</div>
+                </div>
+                <div className="flex justify-evenly items-center text-white border-b border-white font-SpaceGrotesk py-[18px]">
+                    <div><img src="/asset/img/icono-sol.svg" alt="" /></div>
+                    <div className="">
+                        <p className="font-light text-center text-[15px]">Soles Totales</p>
+                        <p className="font-bold text-center text-[40px] mt-[-15px]">4504</p>
+                    </div>
+                </div>
+                <div className="text-center text-white border-b border-white text-[18px] font-bold font-SpaceGrotesk py-[18px]"><span className="font-light">INICIO DE ACTIVIDADES: </span>25 JULIO 2013</div>
+                <div className="flex justify-evenly items-center text-white border-b border-white font-SpaceGrotesk py-[18px]">
+                    <div><img src="/asset/img/icono-camera.svg" alt="" /></div>
+                    <div className="">
+                        <p className="font-light text-center text-[15px]">Fotos Tomadas</p>
+                        <p className="font-bold text-center text-[40px] mt-[-15px]">4504</p>
+                    </div>
+                </div>
+                <div className="text-center text-white border-b border-white text-[12px] font-light font-SpaceGrotesk py-[4px]">Diseñado y desarrollado por: moises.script</div>
             </section>
         </>
     );
